76b21b02fcfa2ce2e7adc52cff01aecd21bd6834
Ruangyot Thanmongkol <ruangyotth@gmail.com>
2019-07-04 15:22:04 +0700

    Add mock data

---
+++ api/mock-api.js
@@ -0,0 +1,34 @@
+const list = [
+    { 
+    "name": "b0006se5bq", 
+    "number": "singing coach unlimited", 
+    "description": "singing coach unlimited - electronic learning products (win me nt 2000 xp)",  
+    "images": [ 
+        { 
+            "url": "http://lorempixel.com/200/200/technics/", 
+            "name": "singing coach"
+        }, 
+        { 
+            "url": "http://lorempixel.com/200/200/abstract/", 
+            "name": "front side"
+        }
+    ]},
+    { 
+    "name": "b00021xhzw", 
+    "number": "adobe after effects professional 6.5 upgrade from standard to professional", 
+    "description": "upgrade only; installation of after effects standard new disk caching tools speed up your interactive work save any combination of animation parameters as presets", 
+    "images": []  
+    },
+    { 
+    "name": "b00021xhzw", 
+    "number": "domino designer/developer v5.0", 
+    "description": "reference domino designer/developer r5 doc pack includes the following titles: application development with domino designer (intermediate-advanced) 536 pages it explains building applications creating databases using forms fields views folders navi", 
+    "images": [
+        { 
+        "url": "http://lorempixel.com/200/200/people/", 
+        "name": "cover"
+        }]
+    }
+]
+    
+export default list;
