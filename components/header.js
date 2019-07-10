35b94bc8da4dc35856efb6897787624fef53611f
Ruangyot Thanmongkol <ruangyotth@gmail.com>
2019-07-10 12:23:27 +0700

    clean up header component

--- components/header.js
+++ components/header.js
@@ -1,14 +1,11 @@
 import { Link } from '../routes';
 
-const linkStyle = {
-    marginRight: 15
-};
 const Header = () => (
-  <div>
-    <Link to="/">
-        <a style={linkStyle}>Index Page</a>
-    </Link>
-  </div>
+    <div>
+        <Link to="/">
+            <a>Index Page</a>
+        </Link>
+    </div>
 );
 
 export default Header;
